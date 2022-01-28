12356f7074b7af751fb080a57b384c71ee017cf8
luiz-rafael <luiz.souzarafael31@gmail.com>
2022-01-27 21:03:58 -0300

    prim

---
+++ teste.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <title></title>
+    <meta charset="utf-8">
+  <style> 
+h1{
+font: size 12px;
+color: blue;
+margin: 0;
+padding: 0;
+}
+</style>   
+</head>
+  <body>
+      <h1> teste </h1>
+  </body>
+</html>
